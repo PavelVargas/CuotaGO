e7dfe0a9a16a8b9463d83018c9c35268f8ac9fbd
unknown <pavelvargas1233@gmail.com>
2026-09-19 12:24:06 -0400

    Aumenta ligeramente los iconos del inicio en PWA y fija el resumen inferior para que no salte al refrescar.

--- cuotago/static/service-worker.js
+++ cuotago/static/service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = '1.12.1-ui-v16';
+const VERSION = '1.12.1-ui-v17';
 const STATIC_CACHE = `cuotago-static-${VERSION}`;
 const RUNTIME_CACHE = `cuotago-runtime-${VERSION}`;
 const CORE = [
