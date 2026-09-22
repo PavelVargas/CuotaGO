845284156714409b32a1967198edca4794e2e0eb
unknown <pavelvargas1233@gmail.com>
2026-09-22 00:45:19 -0400

    Fuerza 6 modulos por fila en el launcher de escritorio corrigiendo la especificidad CSS y actualiza la cache de interfaz.

--- cuotago/static/service-worker.js
+++ cuotago/static/service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = '1.14.2-ui-v30';
+const VERSION = '1.14.2-ui-v31';
 const STATIC_CACHE = `cuotago-static-${VERSION}`;
 const RUNTIME_CACHE = `cuotago-runtime-${VERSION}`;
 const CORE = [
